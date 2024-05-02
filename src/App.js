1c584f97ce3118b21ac56e830e98702e4b73761d
Mustafa Alkamali <m7asanain@gmail.com>
2024-05-02 22:24:17 +0300

    load more button

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import Accordian from "./components/accordian";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
+import LoadMoreData from "./components/load-more-data";
 
 function App() {
   return (
     <div className="App">
       {/* <Accordian /> */}
       {/* <RandomColor /> */}
-      <StarRating numberOfStars={10} />
+      {/* <StarRating numberOfStars={10} /> */}
+      <LoadMoreData />
     </div>
   );
 }
