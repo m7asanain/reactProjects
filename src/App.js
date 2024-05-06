167895a7ad1b390e74b761fd4aba9d419679dc2b
Mustafa Alkamali <m7asanain@gmail.com>
2024-05-06 22:17:49 +0300

    Tree View / Menu UI / Recursive Navigation Menu

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Accordian from "./components/accordian";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 import LoadMoreData from "./components/load-more-data";
+import TreeView from "./components/tree-view/index";
+import menus from "./components/tree-view/data";
 
 function App() {
   return (
@@ -11,7 +13,8 @@ function App() {
       {/* <Accordian /> */}
       {/* <RandomColor /> */}
       {/* <StarRating numberOfStars={10} /> */}
-      <LoadMoreData />
+      {/* <LoadMoreData /> */}
+      <TreeView menus={menus} />
     </div>
   );
 }
