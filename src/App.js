052775c3a42de5ea8b13b62f20c41bbac7695695
Mustafa Alkamali <m7asanain@gmail.com>
2024-06-24 22:27:13 +0300

    Tic Tac Toe

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import TabTest from "./components/custom-tabs/tabs-test";
 import ModalTest from "./components/custom-modal-popup/modal-test";
 import GithubProfileFinder from "./components/github-profile-finder";
 import SearchAutocomplete from "./components/search-autocomplete-with-api";
+import TicTacToe from "./components/tic-tact-toe";
 
 function App() {
   return (
@@ -29,7 +30,8 @@ function App() {
       {/* <TabTest /> */}
       {/* <ModalTest /> */}
       {/* <GithubProfileFinder /> */}
-      <SearchAutocomplete />
+      {/* <SearchAutocomplete /> */}
+      <TicTacToe />
     </div>
   );
 }
